b1dd97e2ac78feab2d359bba829a0b20354175a5
ccachun <ccacchun@gmail.com>
2024-09-15 23:45:57 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/ccachun/springboot_mall/dto/ProductRequest.java
+++ src/main/java/com/ccachun/springboot_mall/dto/ProductRequest.java
@@ -3,8 +3,9 @@ package com.ccachun.springboot_mall.dto;
 import com.ccachun.springboot_mall.constant.ProductCategory;
 import jakarta.validation.constraints.NotNull;
 
-// 由於要在新增商品時，要驗證前端所傳過來的參數，
+// 由於要在新增或修改商品時，要驗證前端所傳過來的參數，
 // 為了不使 Product class 變得太過複雜，所以使用這個 ProductRequest class
+// 提供 createProduct(), updateProduct() 使用
 
 public class ProductRequest {
 
