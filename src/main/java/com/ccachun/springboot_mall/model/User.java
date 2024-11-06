0a3dbac4a02af415f033b2b665b9aee1123b3819
ccachun <ccacchun@gmail.com>
2024-11-06 19:39:54 +0800

    帳號功能 - 檢查註冊的 email、隱藏返回的密碼

--- src/main/java/com/ccachun/springboot_mall/model/User.java
+++ src/main/java/com/ccachun/springboot_mall/model/User.java
@@ -1,12 +1,18 @@
 package com.ccachun.springboot_mall.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Date;
 
 public class User {
 
     private Integer userId;
     private String email;
+
+    @JsonIgnore
     private String password;
+
     private Date createDate;
     private Date lastModifiedDate;
 
