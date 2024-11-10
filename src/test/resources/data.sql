0d5bada3bc4595fbe8616bacb6dd1ae6ce5a5dbd
ccachun <ccacchun@gmail.com>
2024-11-10 19:48:41 +0800

    訂單功能 - 單元測試

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -18,3 +18,17 @@ VALUES ('Benz', 'CAR', 'https://firebasestorage.googleapis.com/v0/b/springboot-m
 
 INSERT INTO product (product_name, category, image_url, price, stock, description, created_date, last_modified_date)
 VALUES ('Tesla', 'CAR', 'https://firebasestorage.googleapis.com/v0/b/springboot-mall-e034e.appspot.com/o/tesla.jpg?alt=media&token=d2c016d0-6236-4aa1-aed1-5aff1bd200d7', 450000, 5, 'The worlds best-selling plug-in car', '2024-09-19 23:30:00', '2024-09-19 23:34:00');
+
+
+-- user
+INSERT INTO `user` (email, password, created_date, last_modified_date) VALUES ('user1@gmail.com', '202cb962ac59075b964b07152d234b70', '2022-06-30 10:30:00', '2022-06-30 10:30:00');
+INSERT INTO `user` (email, password, created_date, last_modified_date) VALUES ('user2@gmail.com', '202cb962ac59075b964b07152d234b70', '2022-06-30 10:40:00', '2022-06-30 10:40:00');
+
+-- order, order_item
+INSERT INTO `order` (user_id, total_amount, created_date, last_modified_date) VALUES (1, 500690, '2022-06-30 11:10:00', '2022-06-30 11:10:00');
+INSERT INTO order_item (order_id, product_id, quantity, amount) VALUES (1, 1, 3, 90);
+INSERT INTO order_item (order_id, product_id, quantity, amount) VALUES (1, 2, 2, 600);
+INSERT INTO order_item (order_id, product_id, quantity, amount) VALUES (1, 5, 1, 500000);
+
+INSERT INTO `order` (user_id, total_amount, created_date, last_modified_date) VALUES (1, 100000, '2022-06-30 12:03:00', '2022-06-30 12:03:00');
+INSERT INTO order_item (order_id, product_id, quantity, amount) VALUES (2, 4, 1, 100000);
